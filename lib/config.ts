52314f6bc09377bd2e9239fee4cd00321998bfae
theone <297190869@qq.com>
2026-03-19 16:17:39 +0800

    feat: add site footer global config

---
+++ lib/config.ts
@@ -0,0 +1,14 @@
+export const siteFooter = {
+  copyright: {
+    name: "My Apps",
+    year: 2026,
+  },
+  contact: {
+    email: "support@example.com",
+    links: [
+      { label: "GitHub", url: "https://github.com/xxx" },
+      { label: "Twitter", url: "https://twitter.com/xxx" },
+    ],
+  },
+  icp: "京ICP备2026000000号-1",
+}
